d0cf5678fdf158c6d7b6d7b4f271bdcf2cb0c002
samikparashar <74339944+samikparashar@users.noreply.github.com>
2026-01-06 18:39:22 +0000

    conversionOp

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 33 
+ let score_new="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score_new)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+//"33"=>  33 
+// "33abc"=>NaN (not a number)
+let isLoggedIn ="samik"
+let booleanisLoggedIn = Boolean(isLoggedIn) ; 
+console.log(booleanisLoggedIn);
+// 1 =>true ; 0 => false 
+// ""=>false , "sam"=>true;
+let someNumber=33 
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+  
